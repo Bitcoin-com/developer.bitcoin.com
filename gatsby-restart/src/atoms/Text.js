47610192fa3acaebaa9cc19f2b893aad826460d1
Peter Zenger <peter.zenger1@gmail.com>
2018-08-16 17:25:33 -0700

    header/footer

---
+++ gatsby-restart/src/atoms/Text.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Text = styled.p`
+`
+
+export default Text;
--- gatsby-restart/src/components/layouts/DefaultLayout.js
+++ gatsby-restart/src/components/layouts/DefaultLayout.js
@@ -1,17 +1,66 @@
 // @flow
 
-import * as React from 'react';
+import * as React from 'react'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
 
-import './base.css';
+import './base.css'
 
 type Props = {
-  children: React.Node
+  children: React.Node,
 }
 
-const DefaultLayout = ({children}: Props) => (
-  <div>
-      {children}
-  </div>
+type Data = {
+  site: { siteMetadata: { title: string} }
+}
+
+const DefaultLayout = ({ children }: Props) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={(data: Data) => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+          <script>
+            var BitcoinMenuWidth = 1152;
+            var BitcoinMenuLang = 'en';
+          </script>
+
+          <script
+            type="text/javascript"
+            src="https://menu.cdn.bitcoindotcom.net/the-footer/dist/universal-footer.js"
+          />
+          <script src="https://menu.cdn.bitcoindotcom.net/the-menu/dist/universal-menu.js" />
+        </Helmet>
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
 )
+
+// const DefaultLayout = ({ children }: Props) => <div>{children}</div>
 
 export default DefaultLayout
