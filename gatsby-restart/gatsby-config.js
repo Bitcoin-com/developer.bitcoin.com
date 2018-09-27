77570c20dd43625aa75369a96e42fd7c24b3bd1c
Peter Zenger <peter.zenger1@gmail.com>
2018-09-27 16:53:28 -0700

    Manifest name

--- gatsby-restart/gatsby-config.js
+++ gatsby-restart/gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'developer-bitcoin-com',
+        short_name: 'developer.bitcoin.com',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#fab915',
