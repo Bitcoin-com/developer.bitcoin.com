038a4bc99a46755bc779ba06fab8b4651fb66b32
Chris Troutner <chris.troutner@gmail.com>
2018-11-01 12:15:38 -0700

    bch faucet front end ported over and working

--- src/components/bch-faucet.js
+++ src/components/bch-faucet.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const SERVER = `http://faucet.christroutner.com`
+
 const Well = styled.p`
   background-color: #f5f5f5;
   min-height: 20px;
@@ -8,6 +10,7 @@ const Well = styled.p`
   margin-bottom: 20px;
   border: 1px solid #e3e3e3;
   border-radius: 4px;
+  white-space: pre-line;
 `
 
 const WrapperDiv = styled.div`
@@ -53,21 +56,59 @@ class BchFaucet extends React.PureComponent {
     )
   }
 
+  // Updates the state as the user updates the input form.
   handleChange = ({ target }) => {
-    //console.log(`${JSON.stringify(target.value,null,2)}`)
-    //const { bchAddr, value } = target;
-    //console.log(`${JSON.stringify(target.value,null,2)}`)
     this.setState({ bchAddr: target.value })
   }
 
-  requestBCH = () => {
-    //console.log(`state.bchAddr: ${this.state.bchAddr}`)
+  requestBCH = async () => {
+    try {
+      //console.log(`state.bchAddr: ${this.state.bchAddr}`)
 
+      this.wipeOutput()
+
+      this.addOutput(`Sending request...`)
+
+      if (this.state.bchAddr === '') {
+        this.addOutput(`Error: BCH Address can not be blank`)
+        return
+      }
+
+      const resp = await fetch(`${SERVER}/coins/${this.state.bchAddr}`)
+      const body = await resp.json()
+      console.log(`body: ${JSON.stringify(body, null, 2)}`)
+
+      if (!body.success) {
+        const message = body.message
+
+        if (message === `Invalid BCH cash address.`)
+          this.addOutput(`Error: Invalid BCH testnet address`)
+        else
+          this.addOutput(
+            `Error: This BCH address has been used before, or you need to wait 24 hours to request from this IP address.`
+          )
+
+        return
+      }
+
+      this.addOutput(`Success: tBCH are on their way!`)
+      this.addOutput(`TXID: ${body.txid}`)
+    } catch (err) {
+      console.log(`Error in requestBCH: `, err)
+    }
+  }
+
+  // Add another line to the output.
+  addOutput = str => {
     this.setState(prevState => ({
-      //outputText: prevState.outputText+' abc '+this.state.bchAddr,
-      outputText:
-        prevState.outputText +
-        `\nSending BCH to address ${this.state.bchAddr}...`,
+      outputText: prevState.outputText + `\n${str}`,
+    }))
+  }
+
+  // Clear the output.
+  wipeOutput = () => {
+    this.setState(prevState => ({
+      outputText: '',
     }))
   }
 }
