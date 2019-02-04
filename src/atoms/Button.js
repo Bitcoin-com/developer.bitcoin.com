191bd8b4a4e3c76b7153f980d54573cb8235b285
Peter Zenger <peter.zenger1@gmail.com>
2019-02-04 13:32:40 -0800

    Minor button updates to make them  look nicer, moving link to faucets up a bit

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -3,11 +3,12 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
+  transition: all 0.15s;
   align-items: center;
-  background-color: ${props => props.theme.primary};
-  border-radius: ${props => (props.round ? '32px' : '3px')};
-  border: none;
-  color: ${props => props.theme.background};
+  background-color: transparent;
+  border-radius: ${props => (props.round ? '25px' : '3px')};
+  border: 2px solid ${props => props.theme.primary500};
+  color: ${props => props.theme.primary500};
   cursor: pointer;
   display: flex;
   font-weight: 800;
@@ -17,6 +18,8 @@ const Button = styled.button`
   font-size: 16px;
   &:hover {
     background-color: ${props => props.theme.primary600};
+    border-color: ${props => props.theme.primary600};
+    color: ${props => props.theme.background};
   }
 `
 
