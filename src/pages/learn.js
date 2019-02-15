b29c21c48a53d2d4ac1cd9c750ce67839b4f5f44
Peter Zenger <peter.zenger1@gmail.com>
2019-02-15 08:34:42 -0800

    Fixing typo in typo PR

--- src/pages/learn.js
+++ src/pages/learn.js
@@ -45,7 +45,7 @@ const Learn = ({ location, data }: Props) => (
         'Learning center and resources for all of your Bitcoin Cash development needs'
       }
       keywords={
-        'learn, bitbox, bitcion cash tutorials, developer tools, bitcoin, bitcoin cash, bitcoin sdk, BCH, sdk, bitcoin cash api'
+        'learn, bitbox, bitcoin cash tutorials, developer tools, bitcoin, bitcoin cash, bitcoin sdk, BCH, sdk, bitcoin cash api'
       }
       location={location}
     />
